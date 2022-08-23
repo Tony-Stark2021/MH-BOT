bcbd92d24b6ff41cfca10d777fe774f4435fca5b
goutham <82017907+Tony-Stark2021@users.noreply.github.com>
2022-08-23 20:51:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,8 +3,8 @@ class script(object):
 """
     HELP_TXT = """ʜᴇʏ {}
 ʜᴇʀᴇ ɪs ᴍʏ ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅs."""
-    ABOUT_TXT = """○ 𝖬𝗒 𝖭𝖺𝗆e : Manavalam wazeem
-○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href='https://t.me/aboutme_DK'>Dᴋ 🇮🇳</a>
+    ABOUT_TXT = """○ 𝖬𝗒 𝖭𝖺𝗆e : Manavalan wazeem
+○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href='https://t.me/+QKAf7_29pAJhODU1'>Dᴋ 🇮🇳</a>
 ○ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : 𝖯𝗒𝗍𝗁𝗈𝗇 𝟥 
 ○ 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : 𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆 𝖺𝗌𝗒𝗇𝖼𝗂𝗈 𝟢.𝟣𝟩.𝟣 
 ○ 𝖲𝖾𝗋𝗏𝖾𝗋 : Contabo
